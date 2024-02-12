31cf1e581d0171ce01246feac9d96c785b0c40ba
Jean-dev-bit <jeanbaptistehouetchowanou@gmail.com>
2024-02-12 13:14:49 +0100

    jour-4

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,13 +1,44 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
+import axios from "axios";
 
 export default function NewProduct() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+
+  async function createProduct() {
+    const data = { title, description, price };
+    await axios.post("/api/products", data);
+  }
   return (
     <Layout>
-      <h1 className=" bg-blue-900 rounded-md text-white py-2 px-2 mb-4">
-        Nouvel Produit
-      </h1>
-      <input type="text" placeholder="Nom Produit" />
-      <textarea placeholder="description"></textarea>
+      <form onSubmit={createProduct}>
+        <h1>Nouvel Produit</h1>
+        <label>Nom Produit</label>
+        <input
+          type="text"
+          placeholder="Nom Produit"
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
+        />
+        <label>Description</label>
+        <textarea
+          placeholder="description"
+          value={description}
+          onChange={(ev) => setDescription(ev.target.value)}
+        />
+        <label>Prix (in USD)</label>
+        <input
+          type="number"
+          placeholder="price"
+          value={price}
+          onChange={(ev) => setPrice(ev.target.value)}
+        />
+        <button type="submit" className="btn-primary">
+          Enregistrer
+        </button>
+      </form>
     </Layout>
   );
 }
